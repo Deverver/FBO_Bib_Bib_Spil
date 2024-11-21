8bdac3b92e481373690ab388bd31df87860ed3f6
Deverver <jackRMrasmussen@gmail.com>
2024-11-21 18:08:54 +0100

    21/11-1808

--- src/uploadFromFile.java
+++ src/uploadFromFile.java
@@ -9,7 +9,10 @@ public class uploadFromFile {
     /*
     This was made, so I could test the program using multiple computers without having to upload small test to my DB
     Only used to quickly add stuff to tables
-    AND yes I know it does not follow DRY
+
+    AND yes I know it does not follow DRY, did not want to make a Variable String with DB Table names, -
+    that would have to be separated and counted to print a variable amount of (?).
+    I would have make something that also checks the dataType and print new prepared statements... too much work for now.
      */
 
     public static void uploadToScoreboards() {
@@ -22,7 +25,8 @@ public class uploadFromFile {
 
             String line;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(","); // Split the line into columns
+                // Since we are taking data from a file and not an Object, we split the String so we can use each part
+                String[] values = line.split(",");
                 String NAME = values[0];
                 int SCORE = Integer.parseInt(values[1].trim());
                 int FK = Integer.parseInt(values[2].trim());
@@ -30,11 +34,13 @@ public class uploadFromFile {
                 preparedStatement.setString(1, NAME);
                 preparedStatement.setInt(2, SCORE);
                 preparedStatement.setInt(3, FK);
-                preparedStatement.addBatch(); // Add to batch
+                // Learned why we have to use a Batch, next comment
+                preparedStatement.addBatch();
             }
 
-            // Learned that you have to do a Batch instead of an Update, this is because our rowsInserted is an array
-            int[] rowsInserted = preparedStatement.executeBatch(); // Execute all inserts at once
+            // Learned that you have to do a Batch instead of an Update, this is because our rowsInserted is an array -
+            // this also makes it easier to get info out like how many Entries we have put into the DB
+            int[] rowsInserted = preparedStatement.executeBatch();
 
             System.out.println(rowsInserted.length + " rows inserted successfully.");
         } catch (SQLException sqle) {
@@ -54,17 +60,17 @@ public class uploadFromFile {
 
             String line;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(","); // Split the line into columns
+                String[] values = line.split(",");
                 String TITLE = values[0];
                 String MAKER = values[1];
 
                 preparedStatement.setString(1, TITLE);
                 preparedStatement.setString(2, MAKER);
 
-                preparedStatement.addBatch(); // Add to batch
+                preparedStatement.addBatch();
             }
 
-            int[] rowsInserted = preparedStatement.executeBatch(); // Execute all inserts at once
+            int[] rowsInserted = preparedStatement.executeBatch();
 
             System.out.println(rowsInserted.length + " rows inserted successfully.");
         } catch (SQLException sqle) {
